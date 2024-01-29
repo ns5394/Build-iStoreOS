9d123269aaacb2b93733ae59e5bd0fd09f7b4b59
NIES <62430434+ns5394@users.noreply.github.com>
2024-01-29 17:57:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-sed -i 's/192.168.100.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.100.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
