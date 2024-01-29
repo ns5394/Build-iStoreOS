fcb51c6ca582423f6ba1ba2d1ef51b7f3b6fc817
NIES <62430434+ns5394@users.noreply.github.com>
2024-01-29 17:58:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,16 @@
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
 # sed -i 's/192.168.100.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
+if [ "${{ matrix.ARCHITECTURE }}" = "X86" ]; then
+  echo "IP_ADDRESS=192.168.5.1" >> $GITHUB_ENV
+  sed -i 's/192.168.100.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+else
+  echo "IP_ADDRESS=192.168.8.1" >> $GITHUB_ENV
+  sed -i 's/192.168.100.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+  exit 1
+fi    
+
+
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 
